3b445d7cce81d76ff7b0b83426f01e8f5d635a2b
Mar Almagro Moreno <maralmagromoreno@gmail.com>
2025-10-28 10:15:06 +0100

    [django-delights]: add full CRUD views for public-facing models

--- django-delights/delights/views.py
+++ django-delights/delights/views.py
@@ -1,3 +1,182 @@
-from django.shortcuts import render
+from django.views.generic import (
+    ListView, DetailView,
+    CreateView, UpdateView, DeleteView
+)
+from django.urls import reverse_lazy
+from .models import (
+    Dish, Menu, Category, Ingredient,
+    Tag, Chef
+)
 
-# Create your views here.
+# ----------- Dish Views -----------
+
+class DishListView(ListView):
+    model = Dish
+    template_name = 'dish_list.html'
+    context_object_name = 'dishes'
+
+class DishDetailView(DetailView):
+    model = Dish
+    template_name = 'dish_detail.html'
+
+class DishCreateView(CreateView):
+    model = Dish
+    fields = '__all__'
+    template_name = 'dish_form.html'
+    success_url = reverse_lazy('dish_list')
+
+class DishUpdateView(UpdateView):
+    model = Dish
+    fields = '__all__'
+    template_name = 'dish_form.html'
+    success_url = reverse_lazy('dish_list')
+
+class DishDeleteView(DeleteView):
+    model = Dish
+    template_name = 'dish_confirm_delete.html'
+    success_url = reverse_lazy('dish_list')
+
+
+# ----------- Menu Views -----------
+
+class MenuListView(ListView):
+    model = Menu
+    template_name = 'menu_list.html'
+    context_object_name = 'menus'
+
+class MenuDetailView(DetailView):
+    model = Menu
+    template_name = 'menu_detail.html'
+
+class MenuCreateView(CreateView):
+    model = Menu
+    fields = '__all__'
+    template_name = 'menu_form.html'
+    success_url = reverse_lazy('menu_list')
+
+class MenuUpdateView(UpdateView):
+    model = Menu
+    fields = '__all__'
+    template_name = 'menu_form.html'
+    success_url = reverse_lazy('menu_list')
+
+class MenuDeleteView(DeleteView):
+    model = Menu
+    template_name = 'menu_confirm_delete.html'
+    success_url = reverse_lazy('menu_list')
+
+
+# ----------- Category Views -----------
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'category_list.html'
+    context_object_name = 'categories'
+
+class CategoryDetailView(DetailView):
+    model = Category
+    template_name = 'category_detail.html'
+
+class CategoryCreateView(CreateView):
+    model = Category
+    fields = '__all__'
+    template_name = 'category_form.html'
+    success_url = reverse_lazy('category_list')
+
+class CategoryUpdateView(UpdateView):
+    model = Category
+    fields = '__all__'
+    template_name = 'category_form.html'
+    success_url = reverse_lazy('category_list')
+
+class CategoryDeleteView(DeleteView):
+    model = Category
+    template_name = 'category_confirm_delete.html'
+    success_url = reverse_lazy('category_list')
+
+
+# ----------- Ingredient Views -----------
+
+class IngredientListView(ListView):
+    model = Ingredient
+    template_name = 'ingredient_list.html'
+    context_object_name = 'ingredients'
+
+class IngredientDetailView(DetailView):
+    model = Ingredient
+    template_name = 'ingredient_detail.html'
+
+class IngredientCreateView(CreateView):
+    model = Ingredient
+    fields = '__all__'
+    template_name = 'ingredient_form.html'
+    success_url = reverse_lazy('ingredient_list')
+
+class IngredientUpdateView(UpdateView):
+    model = Ingredient
+    fields = '__all__'
+    template_name = 'ingredient_form.html'
+    success_url = reverse_lazy('ingredient_list')
+
+class IngredientDeleteView(DeleteView):
+    model = Ingredient
+    template_name = 'ingredient_confirm_delete.html'
+    success_url = reverse_lazy('ingredient_list')
+
+
+# ----------- Tag Views -----------
+
+class TagListView(ListView):
+    model = Tag
+    template_name = 'tag_list.html'
+    context_object_name = 'tags'
+
+class TagDetailView(DetailView):
+    model = Tag
+    template_name = 'tag_detail.html'
+
+class TagCreateView(CreateView):
+    model = Tag
+    fields = '__all__'
+    template_name = 'tag_form.html'
+    success_url = reverse_lazy('tag_list')
+
+class TagUpdateView(UpdateView):
+    model = Tag
+    fields = '__all__'
+    template_name = 'tag_form.html'
+    success_url = reverse_lazy('tag_list')
+
+class TagDeleteView(DeleteView):
+    model = Tag
+    template_name = 'tag_confirm_delete.html'
+    success_url = reverse_lazy('tag_list')
+
+
+# ----------- Chef Views -----------
+
+class ChefListView(ListView):
+    model = Chef
+    template_name = 'chef_list.html'
+    context_object_name = 'chefs'
+
+class ChefDetailView(DetailView):
+    model = Chef
+    template_name = 'chef_detail.html'
+
+class ChefCreateView(CreateView):
+    model = Chef
+    fields = '__all__'
+    template_name = 'chef_form.html'
+    success_url = reverse_lazy('chef_list')
+
+class ChefUpdateView(UpdateView):
+    model = Chef
+    fields = '__all__'
+    template_name = 'chef_form.html'
+    success_url = reverse_lazy('chef_list')
+
+class ChefDeleteView(DeleteView):
+    model = Chef
+    template_name = 'chef_confirm_delete.html'
+    success_url = reverse_lazy('chef_list')
